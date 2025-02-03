ead2be75579037dc1db3da0f89bfa2c03e21dc79
chanspar <qhrn12345@naver.com>
2025-02-03 16:12:10 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -7,29 +7,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-
-
         while (T-- > 0) {
             int n = Integer.parseInt(br.readLine());
             Map<String, Integer> mp = new HashMap<>();
+            
             for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 st.nextToken();
                 String s = st.nextToken();
-                if (mp.get(s) == null) {
-                    mp.put(s, 1);
-                } else {
-                    int a = mp.get(s); 
-                    mp.put(s, ++a);
-                }
+                mp.put(s, mp.getOrDefault(s, 0) + 1);
             }
 
             int ret = 1;
             for (int i : mp.values()) {
-                ret = ret * (i + 1);
+                ret *= (i + 1);
             }
             System.out.println(ret - 1);
-            
         }
     }
 }
