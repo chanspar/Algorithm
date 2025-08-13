f576efd2c19da51d8e5872b125aa2d5d2d52641a
chanspar <qhrn12345@naver.com>
2025-08-13 16:53:33 +0900

    [Silver II] Title: 스택 수열, Time: 144 ms, Memory: 125372 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -2,28 +2,38 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+li = [int(input()) for _ in range(n)]
 
-li = []
-for _ in range(n):
-    a = int(input())
-    li.append(a)
-
-res = []
+stack = []
 result = []
-for i in range(1, n + 1):
-    res.append(i)
-    result.append('+')
-    if li[0] == i:
-        result.append('-')
-        li.pop(0)
-        res.pop()
-        while res and li and li[0] == res[-1]:
-            result.append('-')
-            li.pop(0)
-            res.pop()
+current = 1  # 스택에 push할 다음 숫자
 
-if res:
-    print('NO')
-else:
-    for i in result:
-        print(i)
+# li_index = 0 # 이 부분을 삭제하고 li의 pop(0) 대신 일반적인 for loop 사용
+
+for num in li:
+    # 현재 스택에 넣어야 할 숫자가 li의 현재 숫자보다 작거나 같으면 계속 push
+    while current <= num:
+        stack.append(current)
+        result.append('+')
+        current += 1
+    
+    # 스택의 맨 위 숫자가 li의 현재 숫자와 같으면 pop
+    if stack and stack[-1] == num:
+        stack.pop()
+        result.append('-')
+    else:
+        # 스택의 맨 위 숫자가 li의 현재 숫자와 다르면 스택 수열을 만들 수 없음
+        print('NO')
+        exit()
+
+# 모든 숫자를 처리한 후 스택이 비어있으면 성공
+# 이 코드는 위의 로직에서 이미 모든 스택이 비워졌을 때만 성공하기 때문에 이 조건은 불필요.
+# if not stack:
+#     for op in result:
+#         print(op)
+# else:
+#     print('NO')
+
+# 위의 else에서 이미 프로그램이 종료되므로 아래와 같이 간단하게 출력
+for op in result:
+    print(op)
