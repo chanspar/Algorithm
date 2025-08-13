aa931aa6a0eedae2fa8baae6cdb2abcad154f74e
chanspar <qhrn12345@naver.com>
2025-08-13 16:03:49 +0900

    [Silver II] Title: 키로거, Time: 436 ms, Memory: 222352 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+
+T = int(input())
+
+for _ in range(T):
+    s = input().rstrip()
+    left = []
+    right = []
+
+    for i in s:
+        if i == '<':
+            if left:
+                right.append(left.pop())
+        elif i == '>':
+            if right:
+                left.append(right.pop())
+        elif i == '-':
+            if left:
+                left.pop()
+        else:
+            left.append(i)
+    
+    print(''.join(left + right[::-1]))
