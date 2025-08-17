fdf418faad6cfb3304e26c81c4f323ed16010a4d
chanspar <qhrn12345@naver.com>
2025-08-17 15:36:35 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 292 ms, Memory: 69576 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+li = [ input().split() for _ in range(n)]
+
+li.sort(key=lambda x: (int(x[0]), int(x[1])))
+
+for i in li:
+    print(*i)
