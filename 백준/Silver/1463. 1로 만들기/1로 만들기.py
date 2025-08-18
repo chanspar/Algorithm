43d6484b1e19b3bfc2a9c9b541e4bc57d354ccf1
chanspar <qhrn12345@naver.com>
2025-08-18 15:12:09 +0900

    [Silver III] Title: 1로 만들기, Time: 708 ms, Memory: 71968 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,15 @@
+n = int(input())
+inf = int(1e9)
+
+d = [inf] * 1000001
+d[n] = 0
+
+for i in range(n, 0, -1):
+	if i % 3 == 0:
+		d[i // 3] = min(d[i // 3], d[i] + 1)
+	if i % 2 == 0:
+		d[i // 2] = min(d[i // 2], d[i] + 1)
+	d[i - 1] = min(d[i - 1], d[i] + 1)
+
+print(d[1])
+
