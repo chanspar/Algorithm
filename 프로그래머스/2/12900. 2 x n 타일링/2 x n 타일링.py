6c054d14963cadbbaa4b98745e0b2c14430398b3
chanspar <qhrn12345@naver.com>
2025-09-16 16:48:54 +0900

    [level 2] Title: 2 x n 타일링, Time: 11.50 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -13,4 +13,18 @@ def solution(n): # n 가로길이 (1 <= n <= 60,000)
     for i in range(3, n + 1):
         dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007 
     
+    '''
+    배열x version
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    a, b = 1, 2  # dp[1], dp[2]
+    for _ in range(3, n + 1):
+        a, b = b, (a + b) % 1000000007 
+    return b
+    '''
+        
     return dp[n] 
+
