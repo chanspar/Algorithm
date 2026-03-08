8d6f13b05e533a3ff4ebaaa087c842ddec06ae8f
chanspar <qhrn12345@naver.com>
2026-03-08 23:14:34 +0900

    [Bronze I] Title: ROT13, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.py
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -1,9 +1,19 @@
-s=input()
+# 11655
 
-for i in s:
-	if i.islower():
-		print(chr(ord('a') + (ord(i) + 13 - ord('a')) % 26), end='')
-	elif i.isupper():
-		print(chr(ord('A') + (ord(i) + 13 - ord('A')) % 26), end='')
-	else:
-		print(i,end='')
+
+small_alpha = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+big_alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ' 
+str = input()
+
+result = []
+for c in str:
+    if c in small_alpha:
+        idx = small_alpha.find(c)
+        result.append(small_alpha[idx + 13])
+    elif c in big_alpha:
+        idx = big_alpha.find(c)
+        result.append(big_alpha[idx + 13])
+    else:
+        result.append(c)
+
+print(''.join(result))
