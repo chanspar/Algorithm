316bb77aab338c14b6c7845eac21c3327297f3f5
chanspar <qhrn12345@naver.com>
2026-03-11 18:45:01 +0900

    [Silver IV] Title: 좋은 단어, Time: 108 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.py
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -1,27 +1,19 @@
 n = int(input())
 
-ret = 0
+result = 0
 for _ in range(n):
-	s = input()
-	stack = []
-
-	for c in s:
-		if c not in stack:
-			stack.append(c)
-		elif stack[-1] == c:
-			stack.pop()
-		else:
-			stack.append(c)
-	# print(stack)
-	if not stack:
-		ret += 1
-print(ret)
-
-
-
-# from collections import deque
-# dq = deque()
-# dq.append(1)
-# dq.appendleft(2)
-# dq.pop()
-# dq.popleft()
+    stack = []
+    s = input()
+    for c in s:
+        if not stack:
+            stack.append(c)
+        else:
+            if stack[-1] == c:
+                stack.pop()
+            else:
+                stack.append(c)
+    
+    if not stack:
+        result += 1
+    
+print(result)
