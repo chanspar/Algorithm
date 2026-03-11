d96671f341471d48a7bfff2a9a6b3feb6882450c
chanspar <qhrn12345@naver.com>
2026-03-11 21:17:06 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,31 @@
+import sys
 from collections import deque
-a, b = map(int, input().split(' '))
-array = [list(map(int, input())) for row in range(a)]
-visited = [[0 for col in range(b)] for row in range(a)]
-    
-# print(array)
 
-dy = [1, 0, -1, 0]
+input = sys.stdin.readline
+
+dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
+n,m = map(int, input().split())
+mp = [list(map(int, input().rstrip())) for _ in range(n)]
+visited = [[0 for _ in range(m)] for _ in range(n)]
 
 q = deque()
-q.appendleft((0,0))
+q.append((0, 0))
+visited[0][0] = 1
 
 while q:
-    y, x = q.pop()
-
+    x,y = q.popleft()
     for i in range(4):
-        ny = dy[i] + y
-        nx = dx[i] + x
-        if nx < 0 or ny < 0 or ny >= a or nx >= b: continue
-        if visited[ny][nx] > 0: continue
-        if array[ny][nx] == 1:
-            q.appendleft((ny, nx))
-            visited[ny][nx] = visited[y][x] + 1
+        nx = x + dx[i]
+        ny = y + dy[i]
+    
+        if not (0 <= nx < m and 0 <= ny < n):
+            continue
+        if mp[ny][nx] == 0:
+            continue
+        if visited[ny][nx] != 0:
+            continue
+        visited[ny][nx] = visited[y][x] + 1
+        q.append((nx, ny))
 
-print(visited[a - 1][b - 1] + 1)
+print(visited[n-1][m-1])
