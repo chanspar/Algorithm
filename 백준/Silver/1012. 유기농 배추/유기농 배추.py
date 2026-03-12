2c4931926d3448a007f190254229b5dacef1184f
chanspar <qhrn12345@naver.com>
2026-03-12 21:24:49 +0900

    [Silver II] Title: 유기농 배추, Time: 48 ms, Memory: 32936 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,41 +1,39 @@
 import sys
+
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
-
-dy = [1, 0, -1, 0]
+dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
 
-t = int(input())
+T = int(input())
 
-def go(y, x):
+def dfs(y, x, n, m, mp, visited):
     visited[y][x] = 1
-
+    
     for i in range(4):
         ny = y + dy[i]
         nx = x + dx[i]
-        if ny < 0 or nx < 0 or ny >= n or nx >= m: continue
-        if visited[ny][nx] == 0 and arr[ny][nx] == 1:
-            go(ny,nx)
+        if 0 <= ny < n and 0 <= nx < m:
+            if mp[ny][nx] == 1 and not visited[ny][nx]:
+                dfs(ny, nx, n, m, mp, visited)
 
-
-
-while t > 0:
-    # initial setting
-    m, n, k = map(int, input().split(' '))
-    arr = [[0 for _ in range(m)] for _ in range(n)] 
-    visited = [[0 for _ in range(m)] for _ in range(n)] 
+while T > 0:
+    m,n,k = map(int, input().split())
+    mp = [[0] * m for _ in range(n)]
+    visited = [[0] * m for _ in range(n)]
+    
     for i in range(k):
-        a, b = map(int, input().split(' '))
-        arr[b][a] = 1
-    ret = 0
-
-    # dfs let's go
+        a,b = map(int, input().split())
+        mp[b][a] = 1
+    
+    count = 0
     for i in range(n):
         for j in range(m):
-            if arr[i][j] == 1 and visited[i][j] == 0:
-                go(i,j)
-                ret += 1
-    print(ret)
-    t -= 1
+            if mp[i][j] == 1 and not visited[i][j]:
+                dfs(i, j, n, m, mp, visited)
+                count += 1
+    
+    print(count)    
+    T -= 1
+
 
